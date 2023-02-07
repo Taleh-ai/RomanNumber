6d51a5c6665dd1d09d76b718f5cc5c26b93ecc1b
Taleh-ai <talehrzayev000@gmail.com>
2023-02-07 21:32:35 +0400

    done

--- src/LeetCodeSolution/AddBinary/Solution.java
+++ src/LeetCodeSolution/AddBinary/Solution.java
@@ -4,5 +4,6 @@ class Solution {
     public String addBinary(String a, String b) {
         long binary  = Integer.parseInt(a,2)+ Integer.parseInt(b,2);
         return Integer.toBinaryString(Math.toIntExact(binary));
+
     }
 }
